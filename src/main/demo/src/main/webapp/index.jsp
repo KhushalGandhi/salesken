96e3c17c8e81fa7567f131a3d7e72b2d33377823
KHUSHALGandhi123 <khushal.gandhi@nupay.co.in>
2022-11-30 16:11:22 +0530

    salesken

---
+++ src/main/demo/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+
+localhost:8080/index
---
+++ src/main/java/com/student/springboot/studentservices/demo/src/main/java/3/api/BasicServiceApplication.java
@@ -0,0 +1,30 @@
+package 3.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author: van
+ * @License:
+ * @Contact:
+ * @Date: 2020/9/15 13:57
+ * @Version: 1.0
+ * @Description:
+ */
+@SpringBootApplication
+@ComponentScans({
+        @ComponentScan(value = {"top.cheesetree.btx","cn.jx"})
+})
+@EnableTransactionManagement
+@EnableScheduling
+public class BasicServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicServiceApplication.class, args);
+    }
+
+
+}
